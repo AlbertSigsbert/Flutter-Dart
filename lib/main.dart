98978a7db78a7c0247e8e64e0e600b36c4578763
AlbertSigsbert <ishengomaalbert@gmail.com>
2023-04-27 10:49:09 +0300

    Styling Text

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        //backgroundColor: Color.fromARGB(223, 219, 210, 201),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(95, 92, 87, 87),
+              Color.fromARGB(115, 207, 202, 202)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              'Hello World!',
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
         ),
       ),
     ),
