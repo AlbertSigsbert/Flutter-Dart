3361fd910acbc4ea8d0815c24572b5b56d2084ab
AlbertSigsbert <ishengomaalbert@gmail.com>
2023-04-28 13:21:47 +0300

    Stateful Widgets

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,25 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:first_app/dice_roller.dart';
 // import 'package:first_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
   //constructor
-  const GradientContainer(this.firstColor, this.secondColor,{super.key});
+  const GradientContainer(this.firstColor, this.secondColor, {super.key});
 
   final Color firstColor;
   final Color secondColor;
-
 
   @override
   Widget build(context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(colors: [
-           firstColor,
-           secondColor,
+          firstColor,
+          secondColor,
         ], begin: Alignment.topLeft, end: Alignment.bottomRight),
       ),
-      child: Center(
-        child: Image.asset('assets/images/dice-2.png',width: 200)
+      child: const Center(
+        child:DiceRoller()
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:first_app/gradient_container.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+     MaterialApp(
       home: Scaffold(
           //backgroundColor: Color.fromARGB(223, 219, 210, 201),
           body: GradientContainer(Colors.black38,Colors.white12)),
